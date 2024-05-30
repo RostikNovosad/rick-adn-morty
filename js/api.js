982291dca80f83d5733f7f52b43f3b5eb7e34c30
Rostik Novosad <rostiknovosad4002@gmail.com>
2024-05-30 12:50:46 +0300

    test check

--- js/api.js
+++ js/api.js
@@ -15,7 +15,7 @@ const fetchCharacter = (URL) => {
 const createCharacterCard = (data) => {
   return `
   <li class="card-js" key="${data.id}">
-        <a href="character.html">
+        <a href="#">
           <div class="card">
             <img src="https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg" alt="card-photo" class="card-photo" />
             <h2 class="character-name">${data.name}</h2>
@@ -54,6 +54,15 @@ fetchCharacter("https://rickandmortyapi.com/api/character")
           .then((data) => {
             gridContainer.innerHTML = "";
             data.results.forEach((element) => {
+              data.results.sort((a, b) => {
+                if (a.name > b.name) {
+                  return 1;
+                }
+                if (a.name < b.name) {
+                  return -1;
+                }
+                return 0;
+              });
               if (element.name.includes(inputSearch.value.trim())) {
                 gridContainer.innerHTML += createCharacterCard(element);
               }
@@ -68,3 +77,51 @@ fetchCharacter("https://rickandmortyapi.com/api/character")
   .catch((err) => {
     console.log(err);
   });
+
+// тута айдішка витягується :)
+
+// const cardJs = document.querySelector(".card-js");
+// let cardId = 0;
+// // const keyAttribute = cardJs.getAttribute("key");
+
+// cardJs.addEventListener("click", () => {
+//   console.log(cardJs.getAttribute("key"));
+// });
+
+// function clickHandler(event) {
+//   console.log(event.current.getAttribute("key=[value]"));
+// }
+// cardJs.addEventListener("click", clickHandler);
+
+// це вже безпосереднє створення карточки персонажа, роута не буде бо на карточці стоїть заглушка
+
+// const characterPhoto = document.querySelector(".card-photo");
+// const characterName = document.querySelector(".character-name");
+// const characterGender = document.querySelector(".character_info-gender");
+// const characterStatus = document.querySelector(".character_info-status");
+// const characterSpecie = document.querySelector(".character_info-specie");
+// const characterOrigin = document.querySelector(".character_info-origin");
+// const characterType = document.querySelector(".character_info-type");
+
+// const fetchCharacterProfile = () => {
+//   return fetch(`${BASE_URL}/${id}`).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// };
+
+// fetchCharacterProfile()
+//   .then((data) => {
+//     characterPhoto.src = `https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg`;
+//     characterName.textContent = data.name;
+//     characterGender.textContent = data.gender;
+//     characterStatus.textContent = data.status;
+//     characterSpecie.textContent = data.species;
+//     characterOrigin.textContent = data.origin.name;
+//     characterType.textContent = data.type;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
